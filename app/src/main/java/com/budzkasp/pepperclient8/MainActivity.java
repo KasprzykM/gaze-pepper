fb62423726d43e80a09e536058c5c204b4a4ed35
Michael Kasprzyk <michal0kasprzyk@gmail.com>
2020-06-08 13:55:30 +0200

    Eye detection and small refactor

--- app/src/main/java/com/budzkasp/pepperclient8/MainActivity.java
+++ app/src/main/java/com/budzkasp/pepperclient8/MainActivity.java
@@ -33,6 +33,10 @@ public class MainActivity extends CameraActivity implements CameraBridgeViewBase
 
     private CameraBridgeViewBase mOpenCvCameraView;
     private CascadeClassifier mFaceClassifier;
+    private CascadeClassifier mEyeClassifier;
+
+    private final static String FACE_CASCADE = "haarcascade_frontalface_default.xml";
+    private final static String EYE_CASCADE = "haarcascade_eye.xml";
 
     private Mat mRgba;
     private Mat mGrey;
@@ -44,31 +48,33 @@ public class MainActivity extends CameraActivity implements CameraBridgeViewBase
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS:
                 {
-                    InputStream faceInputStream = getResources().openRawResource(R.raw.haarcascade_frontalface_default);
-//                    InputStream eyeInputStream  = getResources().openRawResource(R.raw.haarcascade_eye);
-                    File faceCascadeDir = getDir("cascade", Context.MODE_PRIVATE);
-                    File mCascadeFile = new File(faceCascadeDir, "haarcascade_frontalface_default.xml");
-                    try {
-                        FileOutputStream outputStream = new FileOutputStream(mCascadeFile);
-                        byte[] buffer = new byte[4096];
-                        int bytesRead;
-                        while((bytesRead = faceInputStream.read(buffer)) != -1)
-                        {
-                            outputStream.write(buffer, 0, bytesRead);
-                        }
-                        faceInputStream.close();
-                        outputStream.close();
-                        mFaceClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
-                        if(mFaceClassifier.empty())
-                        {
-                            Log.e(TAG, "[OpenCV] Failed to load face classifier from" + mCascadeFile.getAbsolutePath());
-                            mFaceClassifier = null;
-                        }else
-                            Log.i(TAG, "[OpenCV] Loaded face classifier from " + mCascadeFile.getAbsolutePath());
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    mFaceClassifier = initializeClassifier(R.raw.haarcascade_frontalface_default, FACE_CASCADE);
+                    mEyeClassifier = initializeClassifier(R.raw.haarcascade_eye, EYE_CASCADE);
+//                    InputStream faceInputStream = getResources().openRawResource(R.raw.haarcascade_frontalface_default);
+////                    InputStream eyeInputStream  = getResources().openRawResource(R.raw.haarcascade_eye);
+//                    File faceCascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+//                    File mCascadeFile = new File(faceCascadeDir, "haarcascade_frontalface_default.xml");
+//                    try {
+//                        FileOutputStream outputStream = new FileOutputStream(mCascadeFile);
+//                        byte[] buffer = new byte[4096];
+//                        int bytesRead;
+//                        while((bytesRead = faceInputStream.read(buffer)) != -1)
+//                        {
+//                            outputStream.write(buffer, 0, bytesRead);
+//                        }
+//                        faceInputStream.close();
+//                        outputStream.close();
+//                        mFaceClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+//                        if(mFaceClassifier.empty())
+//                        {
+//                            Log.e(TAG, "[OpenCV] Failed to load face classifier from" + mCascadeFile.getAbsolutePath());
+//                            mFaceClassifier = null;
+//                        }else
+//                            Log.i(TAG, "[OpenCV] Loaded face classifier from " + mCascadeFile.getAbsolutePath());
+//
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
 
 
                     mOpenCvCameraView.enableView();
@@ -78,6 +84,37 @@ public class MainActivity extends CameraActivity implements CameraBridgeViewBase
                     super.onManagerConnected(status);
                 } break;
             }
+        }
+
+
+        private CascadeClassifier initializeClassifier(int rFileReference, String cascadeFileName)
+        {
+            InputStream faceInputStream = getResources().openRawResource(rFileReference);
+            File faceCascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            File mCascadeFile = new File(faceCascadeDir, cascadeFileName);
+            try {
+                FileOutputStream outputStream = new FileOutputStream(mCascadeFile);
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while((bytesRead = faceInputStream.read(buffer)) != -1)
+                {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+                faceInputStream.close();
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            CascadeClassifier cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+            if(cascadeClassifier.empty())
+            {
+                Log.e(TAG, "[OpenCV] Failed to load face classifier from" + mCascadeFile.getAbsolutePath());
+                cascadeClassifier = null;
+            }else
+                Log.i(TAG, "[OpenCV] Loaded face classifier from " + mCascadeFile.getAbsolutePath());
+
+            return cascadeClassifier;
         }
     };
 
@@ -133,15 +170,27 @@ public class MainActivity extends CameraActivity implements CameraBridgeViewBase
         mGrey = inputFrame.gray();
 
         Imgproc.cvtColor(mRgba, mRgba, Imgproc.COLOR_RGBA2RGB);
+
+        /* Face detection */
         MatOfRect detectedFaces = new MatOfRect();
         mFaceClassifier.detectMultiScale(mRgba, detectedFaces);
-
         for(Rect rect: detectedFaces.toArray())
         {
             Imgproc.rectangle(mRgba, new Point(rect.x, rect.y),
                     new Point(rect.x + rect.width, rect.y + rect.height),
                     new Scalar(0, 255, 0));
         }
+
+        /* Eye detection */
+        MatOfRect detectedEyes = new MatOfRect();
+        mEyeClassifier.detectMultiScale(mRgba, detectedEyes);
+        for(Rect rect: detectedEyes.toArray())
+        {
+            Imgproc.rectangle(mRgba, new Point(rect.x, rect.y),
+                    new Point(rect.x + rect.width, rect.y + rect.height),
+                    new Scalar(0, 0, 255));
+        }
+
 
         return mRgba;
     }
